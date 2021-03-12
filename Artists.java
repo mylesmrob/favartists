010d09a81a0e891aa4d812dac6dca0a61ed44911
mylesmrob <mylesmrobinson@gmail.com>
2021-03-11 22:11:10 -0500

    commit files

---
+++ Artists.java
@@ -0,0 +1,10 @@
+package stuff;
+
+public class Artists {
+
+    public static void main(String[] args) {
+        System.out.println("The Prodigy");
+        System.out.println("Death Grips");
+        System.out.println("Daft Punk");
+    }
+}
